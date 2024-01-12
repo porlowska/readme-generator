447470791a044921100d5629e9a8f8f55870810a
Polly <147508946+porlowska@users.noreply.github.com>
2024-01-12 20:48:09 +0100

    added questions and readme ovierwiew

--- index.js
+++ index.js
@@ -1,21 +1,121 @@
 const fs = require("fs");
-const path = require('path');
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
+let licenseList = ['None','APACHE 2.0', 'BSD 3','GPL 3.0', 'MIT']
 
-];
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Add link to the deployed appliacation',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage: Provide instructions and examples for use.',
+    },
+    {
+        type: 'input',
+        name: 'screeensshot',
+        message: 'If you would like to add screenshot add filepath here',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license are you using?',
+        choices: licenseList
+    },
+    {
+        type: 'input',
+        name: 'contribiuting',
+        message: 'Add guidelines how other developers can add to your project/application',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Write test for your application and provide examples on how to run it.',
+    },
+    {
+        type: 'input',
+        name: 'questionsGit',
+        message: 'provide your GitHub username',
+    },
+    {
+        type: 'input',
+        name: 'questionsEmail',
+        message: 'provide your email address',
+    }
+    ])
+    .then(answers => {
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+        let content = 
+`# ${answers.title} 
 
-// function to initialize program
-function init() {
+## Descripion
+${answers.description} 
 
-}
+## Table of Contents
+    
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
 
-// function call to initialize program
-init();
+## Installation
+${answers.installation} 
+
+[Deployed application](${answers.link})
+
+## Usage 
+${answers.usage}
+
+![Screenshot](${answers.screenshot})
+
+## Usage 
+${answers.credits}
+
+## License
+
+${answers.license}
+
+## Contributing
+
+${answers.contribiuting}
+
+## Tests
+${answers.tests}
+
+
+## Questions
+If you have any additional question, you can find me on GitHub [${answers.questionsGit}](https://github.com/${answers.questionsGit})
+
+or send me an email at: ${answers.questionsEmail}`
+
+        const filename = `${answers.title}-ReadMe.md`
+
+        fs.writeFile(filename, content, ((err) =>
+        err ? console.log(err) : console.log('Success!')))
+    });
